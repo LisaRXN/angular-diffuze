bb9d2fce33008f25ab231c651231852105d5f7e6
Louis Plessis <louisplessis@Louiss-MacBook-Air.local>
2025-03-19 20:42:38 +0100

    try vercel.json

--- api/index.js
+++ api/index.js
@@ -1,27 +1,4 @@
-// Utilisez dynamic import pour plus de compatibilité
-let app;
+import { app } from "../dist/diffuze/server/server.mjs";
 
-async function initApp() {
-  try {
-    const serverModule = await import("../dist/diffuze/server/server.mjs");
-    app = serverModule.app;
-    return app;
-  } catch (error) {
-    console.error("Erreur lors du chargement de l'application:", error);
-    throw error;
-  }
-}
-
-// Exporter une fonction qui gère les requêtes
-module.exports = async (req, res) => {
-  if (!app) {
-    try {
-      app = await initApp();
-    } catch (error) {
-      return res
-        .status(500)
-        .send("Erreur lors de l'initialisation de l'application");
-    }
-  }
-  return app(req, res);
-};
+// Exportez l'application pour que Vercel puisse l'utiliser
+export default app;
