d85d849a90394211141846d23e74d2475e6fd152
lisa erksn <lisa.eriksen@epitech.eu>
2025-03-24 17:56:42 +0100

    fix(property-card): fix renting price

--- src/app/core/adapters/http/http-property.gateway.ts
+++ src/app/core/adapters/http/http-property.gateway.ts
@@ -1,10 +1,13 @@
 import { map, Observable } from 'rxjs';
 import { Property } from '../../models/property.model';
-import { FetchAdResponse, FetchPropertiesResponse, PropertyGateway } from '../../ports/property.gateway';
+import {
+  FetchAdResponse,
+  FetchPropertiesResponse,
+  PropertyGateway,
+} from '../../ports/property.gateway';
 import { inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ContactDetails } from '../../models/contactDetails';
-
 
 export class HttpPropertyGateway extends PropertyGateway {
   http = inject(HttpClient);
@@ -19,28 +22,34 @@ export class HttpPropertyGateway extends PropertyGateway {
 
   override fetchFilteredProperties(filters: any): Observable<FetchAdResponse> {
     let params = new URLSearchParams();
-  
-    params.set("limit", "20");
-    if (filters.page) params.set("page", filters.page);
-    if (filters.propertyType) params.set("propertyType", filters.propertyType);
-    if (filters.transactionType) params.set("transactionType", filters.transactionType);
-    if (filters.minPrice) params.set("minPrice", filters.minPrice);
-    if (filters.maxPrice) params.set("maxPrice", filters.maxPrice);
-    if (filters.minSurface) params.set("minSurface", filters.minSurface);
-    if (filters.maxSurface) params.set("maxSurface", filters.maxSurface);
-    if (filters.localization?.length > 0) params.set("localization", filters.localization.join(','));
-  
+
+    params.set('limit', '20');
+    if (filters.page) params.set('page', filters.page);
+    if (filters.propertyType) params.set('propertyType', filters.propertyType);
+    if (filters.transactionType)
+      params.set('transactionType', filters.transactionType);
+    if (filters.minPrice) params.set('minPrice', filters.minPrice);
+    if (filters.maxPrice) params.set('maxPrice', filters.maxPrice);
+    if (filters.minSurface) params.set('minSurface', filters.minSurface);
+    if (filters.maxSurface) params.set('maxSurface', filters.maxSurface);
+    if (filters.localization?.length > 0)
+      params.set('localization', filters.localization.join(','));
+
     return this.http.get<FetchAdResponse>(
       `https://data.barnabe-immo.fr/api/properties/getAll?${params.toString()}`
     );
   }
-  
+
   override fetchPropertyById(propertyId: string): Observable<Property> {
-    return this.http.get<Property>(`https://data.barnabe-immo.fr/api/properties/id/${propertyId}`)
+    return this.http.get<Property>(
+      `https://data.barnabe-immo.fr/api/properties/id/${propertyId}`
+    );
   }
 
-  override sendPropertyInquiry(contactDetails: ContactDetails): Observable<any> {
-    console.log('message send', contactDetails)
-    return this.http.post('/', contactDetails)
+  override sendPropertyInquiry(
+    contactDetails: ContactDetails
+  ): Observable<any> {
+    console.log('message send', contactDetails);
+    return this.http.post('/', contactDetails);
   }
 }
