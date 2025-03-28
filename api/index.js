3d967461ead69e559883f14362ce1710985b0e4f
Louis Plessis <louisplessis@Louiss-MacBook-Air.local>
2025-03-28 15:06:08 +0100

    test render mode client

--- api/index.js
+++ api/index.js
@@ -1,6 +1,4 @@
 export default async function handler(req, res) {
   const { app } = await import("../dist/diffuze/server/server.mjs");
-
-  // Utilisez la méthode handle de l'application Express
   return app.handle(req, res);
 }
