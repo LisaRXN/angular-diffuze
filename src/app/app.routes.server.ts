16131e91057196dcbbda4141bbcd21769190ba34
Louis Plessis <louisplessis@Louiss-MacBook-Air.local>
2025-03-29 11:07:10 +0100

    test without vercel json

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -27,7 +27,7 @@ export const serverRoutes: ServerRoute[] = [
   },
   {
     path: 'annonces',
-    renderMode: RenderMode.Server,
+    renderMode: RenderMode.Client,
   },
   {
     path: 'blog/:slug', // Articles de blog en SSG avec paramètres
