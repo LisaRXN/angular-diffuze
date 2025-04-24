d61bad4bcbfa316d6d52e5297ea8c55fe33dd498
Lisa Eriksen <lisa.eriksen@epitech.eu>
2025-04-24 19:03:01 +0200

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiURL: 'https://data.barnabe-immo.fr/api',
-  publicURL: 'https://data.barnabe-immo.fr/',
-  prodURL: 'https://data.barnabe-immo.fr/api',
-  stripe: 'pk_live_gb54ARGdQvkz1OTzHJyo1jKQ',
-  googleMapsApiKey: 'AIzaSyAtJPbkwRMQUuUh6h99y4Pvh44RTiX6758',
-  cityScanApiKey: '2e2b2958-9ac0-4228-9b5f-bd050e296d51'
-};
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiURL: 'http://localhost:8080/api',
-  publicURL: 'https://data.barnabe-immo.fr/',
-  prodURL: 'https://data.barnabe-immo.fr/api',
-  stripe: 'pk_test_3RUVi57vagrEzarfq0JKI1B8',
-  googleMapsApiKey: 'AIzaSyAtJPbkwRMQUuUh6h99y4Pvh44RTiX6758',
-  cityScanApiKey: '2e2b2958-9ac0-4228-9b5f-bd050e296d51'
-};
